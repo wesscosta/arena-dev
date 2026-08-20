0addc171feb4679a74463848007b2f4ce2ea8993
Weslley Costa <weslleycosta.ti@gmail.com>
2026-08-20 18:01:36 -0300

    feat: bootstrap Arena Dev v1 architecture

--- src/main/java/br/com/controlealunos/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package br.com.controlealunos.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/controlealunos/exception/RepositoryException.java
+++
@@ -1,7 +0,0 @@
-package br.com.controlealunos.exception;
-
-public class RepositoryException extends RuntimeException {
-    public RepositoryException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
